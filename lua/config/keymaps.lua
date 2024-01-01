77e89080e0166e6ef1e6d91beeb9e3a63fa9cc88
xexex98 <xexex98@gmail.com>
2024-01-02 02:35:05 +0300

    add some keybindings options and autocmds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Windows
+keymap.set("n", "<leader>ww", "<C-W>w", { desc = "Other window" })
