f7c4f0035824e42cab3edefbbb3e5c4d062bf591
xexex98 <xexex98@gmail.com>
2024-01-02 21:29:17 +0300

    unreal

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "graphql",
+        "go",
+        "css",
+        "html",
+        "scss",
+        "svelte",
+        "http",
+        "gitignore",
+        "cpp",
+      },
+    },
+  },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {},
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {},
+  },
+  {
+    "b0o/incline.nvim",
+    opts = {},
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = {
+      scroll = {
+        enable = false,
+      },
+    },
+  },
+}
